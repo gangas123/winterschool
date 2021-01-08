d82140c2d66ec9b5313bf38d2c76e75f3d193b56
gangas123 <77149073+gangas123@users.noreply.github.com>
2021-01-08 15:44:59 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+//First.java 
+import java.applet.*;  
+import java.awt.*;
+
+/*<applet code="First.class" width="300" height="300">  
+</applet> */ 
+public class First extends Applet{  
+    public void paint(Graphics g){ 
+    g.setColor(Color.RED); 
+    g.drawString("Welcome to applets",50,100);  
+    }}  
